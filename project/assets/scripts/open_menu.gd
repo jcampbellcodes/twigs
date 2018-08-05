57360b6fceac696dd181ede73d4cd75b2864f9be
Jack Campbell <jcampbell@uaudio.com>
2018-08-04 19:02:12 -0600

    scene transitions

---
+++ project/assets/scripts/open_menu.gd
@@ -0,0 +1,28 @@
+extends TextureButton
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+var toggle = false
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
+
+func _pressed():
+    toggle = !toggle
+    var inv = get_node("../menu")
+    if(toggle && inv):
+        inv.show()
+    
+    if(!toggle && inv):
+        inv.hide()
+        
+    
