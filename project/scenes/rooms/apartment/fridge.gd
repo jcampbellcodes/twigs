1a90d66556b27d2c334ac12ab120083dc22eaf7f
Jack Campbell <jcampbell@uaudio.com>
2018-08-25 18:29:45 -0600

    little midget man

--- project/scenes/rooms/apartment/fridge.gd
+++ project/scenes/rooms/apartment/fridge.gd
@@ -1,7 +1,7 @@
 extends "res://assets/scripts/entities/item.gd"
 
 func _talk():
-	dialog_static.show_dialog("What's in the fridge eh", "liam", 10.0)
+	dialog_static.show_dialog("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", "liam", 10.0)
 
 func _look():
 	dialog_static.show_dialog("Well would you look at that! A fridge!; Golly!", "liam", 20.0)
